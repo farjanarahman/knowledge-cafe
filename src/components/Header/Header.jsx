c5ce15fd9a4c305d2d25250185d2a29e49291395
farjanarahman <mahfuzurrahman.dev@gmail.com>
2023-04-01 22:41:56 +0600

    bs installed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import './Header.css'
+
 
 const Header = () => {
     return (
         <div>
-            <nav className='nav'>
-                <h2 className='fw-semibold'>Knowledge Cafe</h2>
-                <img src="" alt="" />
+            <nav className="d-flex justify-content-between align-items-center mx-md-5 px-md-5 mt-3">
+                <a className="navbar-brand fs-3 fw-semibold" href="#">
+                    Knowledge Cafe
+                </a>
+                <img className='round' src="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80" width="45px" height="45px" alt="" />
             </nav>
-            <hr/>
+            <hr className='mt-4 container text-center ' />
         </div>
     );
 };
