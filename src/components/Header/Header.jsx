9d014451213bae3eb3b5ce573089a78eb90fc920
farjanarahman <mahfuzurrahman.dev@gmail.com>
2023-04-01 17:04:07 +0600

    nav added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <nav className='nav'>
+                <h2 className='fw-semibold'>Knowledge Cafe</h2>
+                <img src="" alt="" />
+            </nav>
+            <hr />
+        </div>
+    );
+};
+
+export default Header;
